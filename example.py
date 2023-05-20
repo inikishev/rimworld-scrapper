882b519dd78f4a47f649c0d2ca3fe425df8dd696
stunlocked <76593873+stunlocked1@users.noreply.github.com>
2023-05-20 14:03:24 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import RimWorld_Scrapper as rws
 from RimWorld_Scrapper import log
 #uncomment something: 
-#rws.find_duplicates(types = ['ThingDef', 'PawnKindDef', 'RecipeDef'])
+#rws.find_duplicates("path to workshop folder", types = ['ThingDef', 'PawnKindDef', 'RecipeDef'])
 #rws.find_duplicates_between_two_mods("path to priority mod", "path to mod with duplicates")
 #rws.get_all_mod_defs("path to folder", types=['FactionDef'])
 #log.find_sus_mods("[KCSG] Cannot add more symbolDef. Maximum amount reached", 10)
